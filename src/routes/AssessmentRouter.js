3e75a1a59d70b5284c983ca652f50ac5797fe2c0
Huynhkiet2510 <belikeme409@gmail.com>
2025-05-13 00:06:33 +0700

    code mới

---
+++ src/routes/AssessmentRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const AssessmentController = require("../app/controllers/AssessmentsController");
+
+
+router.post("/", AssessmentController.create);
+router.get("/", AssessmentController.getAll);
+router.get("/:id", AssessmentController.getById);
+router.put("/:id", AssessmentController.update);
+router.delete("/:id", AssessmentController.delete);
+
+module.exports = router;
