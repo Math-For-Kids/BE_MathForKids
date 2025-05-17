f2b55ea822d2750d3727f74263fe980921f8101d
PhanHoangPhuc30 <Php3002@gmail.com>
2025-05-17 12:44:46 +0700

    phuc

--- src/app/controllers/RewardController.js
+++ src/app/controllers/RewardController.js
@@ -17,7 +17,7 @@ const db = getFirestore();
 
 class RewardController {
 
-create = async (req, res, next) => {
+  create = async (req, res, next) => {
     try {
       const { name, image, description, isDisabled } = req.body;
       const rewardData = {
@@ -37,7 +37,7 @@ create = async (req, res, next) => {
     }
   };
 
- 
+
   getAll = async (req, res, next) => {
     try {
       const rewardSnapshot = await getDocs(
@@ -51,56 +51,56 @@ create = async (req, res, next) => {
       res.status(400).send({ message: error.message });
     }
   };
-    
-     
-      getById = async (req, res, next) => {
-        try {
-          const id = req.params.id;
-          const reward = doc(db, "reward", id);
-          const data = await getDoc(reward);
-          const rewardData = Reward.fromFirestore(data);
-          if (data.exists()) {
-            res.status(200).send(rewardData);
-          } else {
-            res.status(404).send({ message: "Reward not found!" });
-          }
-        } catch (error) {
-          res.status(400).send({ message: error.message });
-        }
-      };
-    
-        update = async (req, res, next) => {
-          try {
-            const id = req.params.id;
-            const { name, image, description, isDisabled} = req.body;
-            const ref = doc(db, "reward", id);
-            await updateDoc(ref, {
-                name,
-                image,
-                description,
-                isDisabled: isDisabled ?? false,
-              updatedAt: serverTimestamp(),
-            });
-            res
-              .status(200)
-              .send({ message: "Reward updated successfully!" });
-          } catch (error) {
-            res.status(400).send({ message: error.message });
-          }
-        };
 
-          delete = async (req, res, next) => {
-            try {
-              const id = req.params.id;
-              await deleteDoc(doc(db, "reward", id));
-              res
-                .status(200)
-                .send({ message: "Reward deleted successfully!" });
-            } catch (error) {
-              res.status(400).send({ message: error.message });
-            }
-          };
-   
+
+  getById = async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const reward = doc(db, "reward", id);
+      const data = await getDoc(reward);
+      const rewardData = Reward.fromFirestore(data);
+      if (data.exists()) {
+        res.status(200).send(rewardData);
+      } else {
+        res.status(404).send({ message: "Reward not found!" });
+      }
+    } catch (error) {
+      res.status(400).send({ message: error.message });
+    }
+  };
+
+  update = async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const { name, image, description, isDisabled } = req.body;
+      const ref = doc(db, "reward", id);
+      await updateDoc(ref, {
+        name,
+        image,
+        description,
+        isDisabled: isDisabled ?? false,
+        updatedAt: serverTimestamp(),
+      });
+      res
+        .status(200)
+        .send({ message: "Reward updated successfully!" });
+    } catch (error) {
+      res.status(400).send({ message: error.message });
+    }
+  };
+
+  delete = async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      await deleteDoc(doc(db, "reward", id));
+      res
+        .status(200)
+        .send({ message: "Reward deleted successfully!" });
+    } catch (error) {
+      res.status(400).send({ message: error.message });
+    }
+  };
+
 }
 
 module.exports = new RewardController();
