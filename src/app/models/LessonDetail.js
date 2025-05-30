39bf7fbc7674a0e3e4aebb32ca62dcf6d7352939
NguyenThiNga111 <nguyentronghuu.07052003@gmail.com>
2025-05-31 00:51:56 +0700

    Update code for lesson_lessondetails

--- src/app/models/LessonDetail.js
+++ src/app/models/LessonDetail.js
@@ -3,16 +3,10 @@ class LessonDetail {
     id,
     lessonId,
     order,
-    title,
-    content,
-    number1,
-    number2,
-    operator,
-    remember,
-    answer,
-    solution1,
-    solution2,
-    isDisabled,
+    title = {},
+    content = {}, 
+    image = null,
+    isDisabled = false,
     createdAt,
     updatedAt
   ) {
@@ -21,13 +15,7 @@ class LessonDetail {
     this.order = order;
     this.title = title;
     this.content = content;
-    this.number1 = number1;
-    this.number2 = number2;
-    this.operator = operator;
-    this.remember = remember;
-    this.answer = answer;
-    this.solution1 = solution1;
-    this.solution2 = solution2;
+    this.image = image;
     this.isDisabled = isDisabled;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
@@ -39,16 +27,10 @@ class LessonDetail {
       doc.id,
       data.lessonId,
       data.order,
-      data.title,
-      data.content,
-      data.number1,
-      data.number2,
-      data.operator,
-      data.remember,
-      data.answer,
-      data.solution1,
-      data.solution2,
-      data.isDisabled,
+      data.title || {},
+      data.content || {},
+      data.image || null,
+      data.isDisabled || false,
       data.createdAt?.toDate().toLocaleString("vi-VN"),
       data.updatedAt?.toDate().toLocaleString("vi-VN")
     );
